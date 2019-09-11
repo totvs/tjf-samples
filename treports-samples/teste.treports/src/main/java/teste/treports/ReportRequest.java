818c7f48048305484fbe842f469f7642502afd7c
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2019-09-11 18:57:22 -0300

    sample tjf com treports e rac e sample tjf como treport server client

--- treports-samples/teste.treports/src/main/java/teste/treports/ReportRequest.java
+++ treports-samples/teste.treports/src/main/java/teste/treports/ReportRequest.java
@@ -1,6 +1,74 @@
 package teste.treports;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 public class ReportRequest {
-	public String uIdRequest;
-	public String scheduleDate;
+
+	private GenerateParams generateParams = new GenerateParams();
+	private ScheduleParams scheduleParams = new ScheduleParams();
+	private String user = "admin";
+	
+	@JsonProperty("GenerateParams")
+	public GenerateParams getGenerateParams() {
+		return generateParams;
+	}
+
+	@JsonProperty("ScheduleParams")
+	public ScheduleParams getScheduleParams() {
+		return scheduleParams;
+	}
+
+	@JsonProperty("User")
+	public String getUser() {
+		return user;
+	}
+
+	public class GenerateParams {
+		private boolean isView = true; 
+		private boolean stopExecutionOnError = true;
+		Object[] parameters = new Object[0];
+		
+		@JsonProperty("IsView")
+		public boolean isView() {
+			return isView;
+		}
+		
+		@JsonProperty("StopExecutionOnError")
+		public boolean isStopExecutionOnError() {
+			return stopExecutionOnError;
+		}
+		
+		@JsonProperty("Parameters")
+		public Object[] getParameters() {
+			return parameters;
+		}
+	}	
+	
+	public class ScheduleParams {
+		private int type = 0;
+	
+		@JsonProperty("Type")
+		public int getType() {
+			return type;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		
+		String json = null;
+		
+		try {
+			json = ow.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+			
+		return json;
+	}
 }
---
+++ treports-samples/teste.treports/src/main/java/teste/treports/ReportResponse.java
@@ -0,0 +1,6 @@
+package teste.treports;
+
+public class ReportResponse {
+	public String uIdRequest;
+	public String scheduleDate;
+}
