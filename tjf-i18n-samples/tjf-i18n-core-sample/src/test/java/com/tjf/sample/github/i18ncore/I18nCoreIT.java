bf24cf544c4d57daa8f63aa1b9a6a66d672d6961
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2019-10-01 17:39:31 -0300

    Testes integrado i18n

---
+++ tjf-i18n-samples/tjf-i18n-core-sample/src/test/java/com/tjf/sample/github/i18ncore/I18nCoreIT.java
@@ -0,0 +1,57 @@
+package com.tjf.sample.github.i18ncore;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tjf.sample.github.i18ncore.domain.Starship;
+import com.tjf.sample.github.i18ncore.messages.StarshipMessage;
+import com.tjf.sample.github.i18ncore.services.StarshipService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {I18nCoreApplication.class})
+public class I18nCoreIT {
+
+	@Autowired
+	StarshipService starshipService;
+
+	@Autowired
+	StarshipMessage starshipMessage;
+
+	String name;
+
+	@Before
+	public void prepare() throws IOException {
+		Starship starship = starshipService.getStarshipInfo("Falcon.json");
+		name = starship.getName();
+	}
+
+	@Test
+	public void i18n_pt_br_test() throws IOException {
+
+		Locale.setDefault(new Locale("pt", "br"));
+		assertEquals("sNave Millennium Falcon, pouso autorizado", starshipMessage.starshipConfirmLanding(name));
+	}
+
+	@Test
+	public void i18n_en_us_test() throws IOException {
+
+		Locale.setDefault(new Locale("en", "us"));
+		assertEquals("Starship Millennium Falcon, authorized landing", starshipMessage.starshipConfirmLanding(name));
+	}
+
+	@Test
+	public void i18n_es_es_test() throws IOException {
+
+		Locale.setDefault(new Locale("es", "es"));
+		assertEquals("Nave Millennium Falcon, aterrizaje autorizado", starshipMessage.starshipConfirmLanding(name));
+	}
+}
