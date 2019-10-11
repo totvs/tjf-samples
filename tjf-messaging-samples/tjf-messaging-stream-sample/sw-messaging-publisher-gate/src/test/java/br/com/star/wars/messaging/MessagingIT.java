0ae11936bf56238950698b2cf295c6e18eed7df0
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2019-10-11 13:15:39 -0300

    teste integrado

--- tjf-messaging-samples/tjf-messaging-stream-sample/sw-messaging-publisher-gate/src/test/java/br/com/star/wars/messaging/MessagingIT.java
+++ tjf-messaging-samples/tjf-messaging-stream-sample/sw-messaging-publisher-gate/src/test/java/br/com/star/wars/messaging/MessagingIT.java
@@ -1,13 +1,8 @@
 package br.com.star.wars.messaging;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Collections;
 import java.util.HashMap;
 
 import org.junit.Test;
@@ -16,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import br.com.star.wars.messaging.services.StarShipService;
 
@@ -26,74 +21,51 @@ import br.com.star.wars.messaging.services.StarShipService;
 @AutoConfigureMockMvc
 public class MessagingIT {
 
+	public static final String ARRIVED_URL = "/starship/arrived";
+	
 	@Autowired
 	MockMvc mockMvc;
-	
+
 	@Autowired
 	StarShipService starShipService;
 
-	//@Test(timeout = 10000)
-	@Test
+	@Test(timeout = 10000)
 	public void messagingTest() throws Exception {
 
-		String tenant = "abc";
-		
-		mockMvc.perform(get("/starship/arrived")
-				.param("name", "nave")
-				.param("tenant", tenant))
-				.andExpect(status().isOk());
-		
-		HashMap<String, Integer> counter = waitCounter(0, tenant, 1);
-		
-		assertTrue(counter.get(tenant).equals(1));
+		sendArrivedMessaging("abc");
+		expectedCounter("abc", 1);
 
-		assertNotEquals(counter.size(), 0);
-		assertTrue(counter.get(tenant).equals(1));
-		mockMvc.perform(get("/starship/arrived")
-				.param("name", "nave")
-				.param("tenant", tenant))
-				.andExpect(status().isOk());
-		
-		counter = waitCounter(0, tenant, 2);
-		
-		assertTrue(counter.get(tenant).equals(2));
-		
-		tenant = "def";
-		
-		assertNotEquals(counter.size(), 0);
-		assertTrue(counter.get(tenant).equals(1));
-		mockMvc.perform(get("/starship/arrived")
-				.param("name", "nave")
-				.param("tenant", tenant))
-				.andExpect(status().isOk());
-		
-		counter = waitCounter(1, tenant, 1);
-		
-		assertTrue(counter.get(tenant).equals(1));
-		
-		assertNotEquals(counter.size(), 0);
-		assertTrue(counter.get(tenant).equals(1));
-		mockMvc.perform(get("/starship/arrived")
-				.param("name", "nave")
-				.param("tenant", tenant))
-				.andExpect(status().isOk());
-		
-		counter = waitCounter(1, tenant, 2);
-		
-		assertTrue(counter.get(tenant).equals(2));
+		sendArrivedMessaging("abc");
+		expectedCounter("abc", 2);
+
+		sendArrivedMessaging("def");
+		expectedCounter("def", 1);
+
+		sendArrivedMessaging("def");
+		expectedCounter("def", 2);
 	}
-	
-	private HashMap<String, Integer> waitCounter(int size, String tenant, int amount) throws InterruptedException {
-		
+
+	private void expectedCounter(String tenant, int amount) throws InterruptedException {
+
 		HashMap<String, Integer> counter = new HashMap<>();
-		
-		// Aguarda mgs ser processada
-		while(counter.size() <= size || counter.get(tenant) < amount) {
+
+		// Aguarda mensagem ser processada
+		while (counter.get(tenant) == null || counter.get(tenant) < amount) {			
 			counter = starShipService.getCounter();
-			if(counter.size() <= size || counter.get(tenant) < amount)
+			if (counter.get(tenant) == null || counter.get(tenant) < amount)
 				Thread.sleep(100);
 		}
-		
-		return counter;
+	}
+
+	private void sendArrivedMessaging(String tenant) throws Exception {
+
+		mockMvc.perform(getRequest(ARRIVED_URL)
+				.param("tenant", tenant))
+			.andExpect(status().isOk());
+	}
+
+	private MockHttpServletRequestBuilder getRequest(String url) {
+		return get(url)
+				.param("name", "nave");
 	}
 }
