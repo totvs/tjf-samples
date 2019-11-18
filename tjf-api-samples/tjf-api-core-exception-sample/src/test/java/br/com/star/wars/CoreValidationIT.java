678c29c061efcf7d18ce86cd730675cb47b061ff
Luca Fenris <lucaelert@gmail.com>
2019-11-18 15:28:58 -0300

    test: Testes Integrados samples API

---
+++ tjf-api-samples/tjf-api-core-exception-sample/src/test/java/br/com/star/wars/CoreValidationIT.java
@@ -0,0 +1,68 @@
+package br.com.star.wars;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CoreValidationIT {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	TestRestTemplate template;
+
+	@Test
+	public void createStarshipWithoutExcpetionTest() throws Exception {
+		String expectedResult = "{\n" + "    \"starship\": \"created\"\n" + "}";
+
+		mockMvc.perform(post("/api/v1/starship/create").contentType(MediaType.APPLICATION_JSON)
+				.content("{\n" + "    \"name\": \"Millenium Falcon\",\n" + "    \"description\": \"Nave do Han\",\n"
+						+ "    \"crew\": 5\n" + "}"))
+				.andExpect(status().isCreated()).andExpect(content().json(expectedResult));
+	}
+
+	@Test
+	public void createStarshipWithExceptionTest() throws JSONException, URISyntaxException {
+
+		HttpHeaders headers = new HttpHeaders();
+
+		String expectedResult = "{\"code\":\"StarshipCreateConstraintException\",\"message\":\"É uma armadilha\",\"detailedMessage\":\"A força não está com você\",\"details\":[{\"code\":\"Starship.description.Size\",\"message\":\"A descrição da nave não deve ser menor que 1 ou maior que 15\",\"detailedMessage\":\"description: A sucata mais veloz da galáxia\"}]}";
+		final String baseUrl = "/api/v1/starship/create";
+		URI uri = new URI(baseUrl);
+
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity<String>(
+				"{\n" + "    \"name\": \"Millenium Falcon\",\n"
+						+ "    \"description\": \"A sucata mais veloz da galáxia\",\n" + "    \"crew\": 5\n" + "}",
+				headers);
+
+		ResponseEntity<String> result = template.postForEntity(uri, entity, String.class);
+
+		assertTrue(result.getBody().equals(expectedResult));
+
+	}
+}
