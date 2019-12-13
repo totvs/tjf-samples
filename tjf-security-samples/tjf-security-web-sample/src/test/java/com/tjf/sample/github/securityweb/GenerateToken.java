346996b5c44eba2c243f90769f3d4ea613ee862e
diegonsilveira <diegonsilveira@hotmail.com>
2019-12-13 19:02:29 -0300

    Complementar teste integrado do tjf-security-web

---
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/GenerateToken.java
@@ -0,0 +1,84 @@
+package com.tjf.sample.github.securityweb;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class GenerateToken {
+	
+	public String generateToken(String url, String clientId, String clientSecret, String username, String password) throws ClientProtocolException {
+		String token = "";
+		
+		try (CloseableHttpClient client = HttpClients.createDefault()) {
+			HttpPost post = new HttpPost(url);
+			
+			StringEntity text = new StringEntity("client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=password&username=" + username + "&password=" + password + "&scope=authorization_api&acr_values=tenant:empresa1");
+			
+			post.setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+			post.setHeader("charset", "UTF-8");
+
+			post.setEntity(text);
+			
+			HttpResponse response = client.execute(post);
+
+			HttpEntity entity = response.getEntity();
+
+			if (entity != null) {
+				String retSrc = EntityUtils.toString(entity);
+				JSONObject result = new JSONObject(retSrc);
+												
+				return token = result.getString("access_token");				
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return token;
+	}
+
+	public void updateRolesUser(String url, String accessApplicationToken) {
+		try (CloseableHttpClient client = HttpClients.createDefault()) {
+			HttpPut put = new HttpPut(url + "/api/user/2");
+			
+			StringEntity params = new StringEntity("{\r\n" + 
+					"    \"id\": 2,\r\n" + 
+					"    \"userName\": \"admin\",\r\n" + 
+					"    \"emailAddress\": \"admin@seudominio.com.br\",\r\n" + 
+					"    \"fullName\": \"Administrador do Tenant\",\r\n" + 
+					"    \"name\": \"Administrador\",\r\n" + 
+					"    \"surname\": \"do Tenant\",\r\n" + 
+					"    \"phoneNumber\": null,\r\n" + 
+					"    \"isUndefinedPassword\": false,\r\n" + 
+					"    \"sentEmailSuccessfully\": true,\r\n" + 
+					"    \"isActive\": true,\r\n" + 
+					"    \"externalId\": null,\r\n" + 
+					"    \"organizations\": [],\r\n" + 
+					"    \"roles\": [\r\n" + 
+					"        2,\r\n" + 
+					"        4\r\n" + 
+					"    ],\r\n" + 
+					"    \"productRoles\": [],\r\n" + 
+					"    \"_expandables\": []\r\n" + 
+					"}");
+				
+			put.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken);
+			put.setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+			put.setHeader("charset", "UTF-8");
+			put.setEntity(params);
+
+			client.execute(put);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
--- tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/TJFSecurityTest.java
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/TJFSecurityTest.java
@@ -3,6 +3,8 @@ package com.tjf.sample.github.securityweb;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.ClientProtocolException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.totvs.tjf.mock.test.RacEmulator;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,13 +22,26 @@ public class TJFSecurityTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	private static RacEmulator racEmulator;
-	private static String wrongToken;
+	private static String accessApplicationToken;
 
+	private static final String URL = "http://localhost:5009/totvs.rac/connect/token";
+
+	private static final String CLIENT_ID = "js_oidc_sampleapp";
+
+	private static final String CLIENT_SECRET = "totvs@123";
+	
 	@BeforeClass
-	public static void start() {
-		racEmulator = RacEmulator.getInstance();
-		racEmulator.generateRSAKeys();
+	public static void setup() {
+		GenerateToken racAuthorization = new GenerateToken();
+		
+		try {
+			
+			accessApplicationToken = racAuthorization.generateToken(URL, CLIENT_ID, CLIENT_SECRET, "admin", "totvs@123");
+			racAuthorization.updateRolesUser(URL, accessApplicationToken);
+			
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
@@ -38,27 +51,7 @@ public class TJFSecurityTest {
 
 	@Test
 	public void testValidRole() throws Exception {
-		String jwt = racEmulator.generateJWT("super", "SUPERVISOR");
-		mockMvc.perform(post("/api/v1/machine/stop").header(RacEmulator.HEADER_STRING, jwt)).andExpect(status().isOk());
-	}
-
-	@Test
-	public void testForbiddenRole() throws Exception {
-		String jwt = racEmulator.generateJWT("john", "USER");
-		mockMvc.perform(post("/api/v1/machine/stop").header(RacEmulator.HEADER_STRING, jwt))
-				.andExpect(status().isForbidden());
-	}
-
-	@Test
-	public void testValidProductRole() throws Exception {
-		String jwt = racEmulator.generateJWT("super", "", new String[] { "SUPERVISOR" });
-		mockMvc.perform(post("/api/v1/machine/stop").header(RacEmulator.HEADER_STRING, jwt)).andExpect(status().isOk());
-	}
-
-	@Test
-	public void testForbiddenProductRole() throws Exception {
-		String jwt = racEmulator.generateJWT("john", "", new String[] { "USER" });
-		mockMvc.perform(post("/api/v1/machine/stop").header(RacEmulator.HEADER_STRING, jwt))
-				.andExpect(status().isForbidden());
+		mockMvc.perform(post("/api/v1/machine/stop").header(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken))
+				.andExpect(status().isOk());
 	}
 }
