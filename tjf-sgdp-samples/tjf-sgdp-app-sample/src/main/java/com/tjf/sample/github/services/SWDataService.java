619ef12161d5ccc2d5b1518c28edeba0c62f5d8d
Luca Fenris <lucaelert@gmail.com>
2020-10-13 13:38:53 -0300

    fix: Atualizar sample do SGDP conforme SDK

--- tjf-sgdp-samples/tjf-sgdp-app-sample/src/main/java/com/tjf/sample/github/services/SWDataService.java
+++ tjf-sgdp-samples/tjf-sgdp-app-sample/src/main/java/com/tjf/sample/github/services/SWDataService.java
@@ -13,18 +13,17 @@ import com.tjf.sample.github.model.Jedi;
 import com.tjf.sample.github.model.JediRepository;
 import com.totvs.sgdp.sdk.config.SGDPMetadata;
 import com.totvs.sgdp.sdk.services.data.SGDPDataCommand;
-import com.totvs.sgdp.sdk.services.data.SGDPDataResponse;
 import com.totvs.sgdp.sdk.services.data.SGDPDataService;
 
 @Component
 @Transactional
-public class SWDataService implements SGDPDataService {
+public class SWDataService extends SGDPDataService {
 
 	@Autowired
 	private JediRepository jediRepository;
 
 	@Override
-	public SGDPDataResponse execute(SGDPDataCommand command, SGDPMetadata metadata) {
+	public void execute(SGDPDataCommand command, SGDPMetadata metadata) {
 		int identification = Integer.parseInt(command.getIdentifiers().get("identification"));
 		List<Jedi> list = jediRepository.findByIdentificationEquals(identification);
 		ObjectMapper mapper = new ObjectMapper();
@@ -34,9 +33,8 @@ public class SWDataService implements SGDPDataService {
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
-		SGDPDataResponse response = new SGDPDataResponse(command);
-		response.getData().put("data", mapper.valueToTree(list));
-		return response;
+
+		addData(list, metadata);
 	}
 
 }
--- tjf-sgdp-samples/tjf-sgdp-app-sample/src/main/java/com/tjf/sample/github/services/SWMaskService.java
+++ tjf-sgdp-samples/tjf-sgdp-app-sample/src/main/java/com/tjf/sample/github/services/SWMaskService.java
@@ -14,30 +14,31 @@ import com.tjf.sample.github.model.JediRepository;
 import com.totvs.sgdp.sdk.config.SGDPMetadata;
 import com.totvs.sgdp.sdk.services.mask.SGDPMaskCommand;
 import com.totvs.sgdp.sdk.services.mask.SGDPMaskException;
-import com.totvs.sgdp.sdk.services.mask.SGDPMaskResponse;
 import com.totvs.sgdp.sdk.services.mask.SGDPMaskService;
 
 @Component
 @Transactional
-public class SWMaskService implements SGDPMaskService {
+public class SWMaskService extends SGDPMaskService {
 
 	@Autowired
 	private JediRepository jediRepository;
 
 	@Override
-	public SGDPMaskResponse execute(SGDPMaskCommand command, SGDPMetadata metadata) {
+	public void execute(SGDPMaskCommand command, SGDPMetadata metadata) {
 		int identification = Integer.parseInt(command.getIdentifiers().get("identification"));
 		List<Jedi> list = jediRepository.findByIdentificationEquals(identification);
 		ObjectMapper mapper = new ObjectMapper();
+
 		System.out.println("***** MASK COMMAND *****");
 		try {
 			System.out.println(mapper.writeValueAsString(list));
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
+
 		list.forEach((jedi) -> {
 			try {
-				mask(jedi, metadata);
+				mask(jedi, metadata, command.getToVerify());
 				try {
 					System.out.println(mapper.writeValueAsString(jedi));
 				} catch (JsonProcessingException e) {
@@ -48,9 +49,6 @@ public class SWMaskService implements SGDPMaskService {
 				e.printStackTrace();
 			}
 		});
-		SGDPMaskResponse response = new SGDPMaskResponse(command);
-		response.setRows(list.size());
-		return response;
 	}
 
 }
