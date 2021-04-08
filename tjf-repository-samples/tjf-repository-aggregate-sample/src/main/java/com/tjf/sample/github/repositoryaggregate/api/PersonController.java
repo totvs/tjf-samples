85dc3e8a9542608b63005e57959387a9ba699673
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2021-04-08 10:56:59 -0300

    adicionado método findall com pageable no sample do tjf-repository-aggregate

--- tjf-repository-samples/tjf-repository-aggregate-sample/src/main/java/com/tjf/sample/github/repositoryaggregate/api/PersonController.java
+++ tjf-repository-samples/tjf-repository-aggregate-sample/src/main/java/com/tjf/sample/github/repositoryaggregate/api/PersonController.java
@@ -1,7 +1,11 @@
 package com.tjf.sample.github.repositoryaggregate.api;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tjf.sample.github.repositoryaggregate.model.Person;
 import com.tjf.sample.github.repositoryaggregate.repository.PersonRepository;
+import com.totvs.tjf.core.aggregate.repository.AggregateCollectionResult;
 
 @RestController
 @RequestMapping(path = "api/v1/person", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -22,4 +27,8 @@ public class PersonController {
 		repository.insert(person);
 	}
 
+	@GetMapping
+	public AggregateCollectionResult<Person> readPageable(@NotNull final Pageable pageable) {
+		return repository.findAll(pageable, null, null);
+	}
 }
