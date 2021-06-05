50c086692ada11562d43c030e4e52a8a17d5e1b3
Marcos Paulo dos Santos <marcos.paulo@totvs.com.br>
2021-06-05 20:07:06 -0300

    TJF-1058 - Criar facilitadores para o envio de mensagens CloudEvents. (#32)
    * teste usando cloudevents-spring
    
    * movido classes do cloudevent para o tjf-messaging
    
    * documentação de uso de cloudevents conforme RFC000011
    
    * removida alterações indevidas no docker-compose.yml
    
    * adicionar o envio de cloudevent
    
    * Sample envio cloudevent
    
    * Update README.md
    
    * ajuste config tjf-security-web
    
    * ajuste samples messaging com cloudevent

--- tjf-messaging-samples/tjf-messaging-stream-sample/tjf-messaging-stream-publisher-sample/src/main/java/com/tjf/sample/github/messaging/infrastructure/messaging/StarShipPublisher.java
+++ tjf-messaging-samples/tjf-messaging-stream-sample/tjf-messaging-stream-publisher-sample/src/main/java/com/tjf/sample/github/messaging/infrastructure/messaging/StarShipPublisher.java
@@ -1,17 +1,29 @@
 package com.tjf.sample.github.messaging.infrastructure.messaging;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
+import java.net.URI;
+import java.util.UUID;
 
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.totvs.tjf.core.message.TOTVSMessage;
 import com.totvs.tjf.core.message.TransactionInfo;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.jackson.JsonCloudEventData;
 
 @EnableBinding(StarShipExchange.class)
 public class StarShipPublisher {
 
 	StarShipExchange exchange;
+	ObjectMapper mapper;
 
-	public StarShipPublisher(StarShipExchange exchange) {
+	public StarShipPublisher(StarShipExchange exchange, ObjectMapper mapper) {
 		this.exchange = exchange;
+		this.mapper = mapper;
 	}
 
 	public <T> void publish(T event, String eventName) {
@@ -22,5 +34,18 @@ public class StarShipPublisher {
 	public <T> void publish(T event, String eventName, TransactionInfo transactionInfo) {
 
 		new TOTVSMessage<T>(eventName, event, transactionInfo).sendTo(exchange.output());
+	}
+
+	public <T> void publishCloudEvent(T event, String eventName, String id) {
+		var messageId = UUID.randomUUID().toString();
+		var data = JsonCloudEventData.wrap(mapper.valueToTree(event));
+		var cloudEvent = CloudEventBuilder.v1()
+				.withId(messageId)
+				.withType(eventName)
+				.withSource(URI.create(id))
+				.withData(data)
+				.build();
+
+		exchange.output().send(MessageBuilder.withPayload(cloudEvent).build());
 	}
 }
