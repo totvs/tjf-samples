946deff30edeb3cabff1f9e36e8c10549aafc164
DanielPeregrinoTotvs <117866234+DanielPeregrinoTotvs@users.noreply.github.com>
2023-03-24 17:26:23 -0300

    Removendo exemplo de filtro complexo (#52)
    * Removendo exemplo de filtro complexo
    
    * Comentando a anotação ApiGuideline
    
    * Removendo atributo created da AccountModel
    
    * Corrigindo número da versão do TJF

--- tjf-api-samples/tjf-api-jpa-filters-sample/src/main/java/com/tjf/sample/github/apijpa/filter/controller/SFController.java
+++ tjf-api-samples/tjf-api-jpa-filters-sample/src/main/java/com/tjf/sample/github/apijpa/filter/controller/SFController.java
@@ -1,25 +1,23 @@
 package com.tjf.sample.github.apijpa.filter.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.tjf.sample.github.apijpa.filter.model.AccountModel;
 import com.tjf.sample.github.apijpa.filter.model.AccountModelRepository;
 import com.tjf.sample.github.apijpa.filter.model.EmployeeModel;
 import com.tjf.sample.github.apijpa.filter.model.EmployeeModelRepository;
-import com.totvs.tjf.api.context.stereotype.ApiGuideline;
-import com.totvs.tjf.api.context.stereotype.ApiGuideline.ApiGuidelineVersion;
-import com.totvs.tjf.api.context.v2.request.ApiFieldRequest;
-import com.totvs.tjf.api.context.v2.request.ApiPageRequest;
-import com.totvs.tjf.api.context.v2.request.ApiSortRequest;
-import com.totvs.tjf.api.context.v2.response.ApiCollectionResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//
+//import com.totvs.tjf.api.context.stereotype.ApiGuideline;
+//import com.totvs.tjf.api.context.stereotype.ApiGuideline.ApiGuidelineVersion;
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-@ApiGuideline(ApiGuidelineVersion.V2)
+//@ApiGuideline(ApiGuidelineVersion.V2)
 public class SFController {
 
 	@Autowired
@@ -29,15 +27,13 @@ public class SFController {
 	private AccountModelRepository accountRepos;
 
 	@GetMapping(path = "/accounts")
-	public ApiCollectionResponse<AccountModel> getAllAccounts(ApiFieldRequest field, ApiPageRequest page,
-			ApiSortRequest sort, AccountModel simpleFilter) {
-		return ApiCollectionResponse.from(accountRepos.findAllProjected(field, page, sort, simpleFilter));
+	public Page<AccountModel> getAllAccounts(Pageable pageable, AccountModel simpleFilter) {
+		return accountRepos.findAll(simpleFilter, pageable);
 	}
 
 	@GetMapping(path = "/employees")
-	public ApiCollectionResponse<EmployeeModel> getAllEmployess(ApiFieldRequest field, ApiPageRequest page,
-			ApiSortRequest sort, EmployeeModel simpleFilter) {
-		return ApiCollectionResponse.from(employeeRepos.findAllProjected(field, page, sort, simpleFilter));
+	public Page<EmployeeModel> getAllEmployess(Pageable pageable, EmployeeModel simpleFilter) {
+		return employeeRepos.findAll(simpleFilter, pageable);
 	}
 
 }
