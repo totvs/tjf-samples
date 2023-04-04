d17f70046e6adc49f287169e44647ebcb3cf9f9d
SP01\daniel.peregrino <daniel.peregrino@totvs.com.br>
2023-04-04 08:36:47 -0300

    Adicionando testes unitários.

--- tjf-messaging-samples/tjf-messaging-amqp-sample/tjf-messaging-amqp-publisher/src/main/java/com/tjf/sample/github/messaging/amqp/events/StarShipArrivedEvent.java
+++ tjf-messaging-samples/tjf-messaging-amqp-sample/tjf-messaging-amqp-publisher/src/main/java/com/tjf/sample/github/messaging/amqp/events/StarShipArrivedEvent.java
@@ -2,10 +2,14 @@ package com.tjf.sample.github.messaging.amqp.events;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class StarShipArrivedEvent {
+public class StarShipArrivedEvent implements Serializable {
 
     private String name;
 }
---
+++ tjf-messaging-samples/tjf-messaging-amqp-sample/tjf-messaging-amqp-subscriber/src/test/java/com/tjf/sample/github/messaging/amqp/AmqpSubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjf.sample.github.messaging.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpSubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
