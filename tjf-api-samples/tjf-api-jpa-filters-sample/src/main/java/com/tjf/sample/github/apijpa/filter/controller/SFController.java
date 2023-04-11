583a5b92107e04d4d75320c6a5a79c47bdeef314
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2023-04-11 13:42:04 -0300

    Fix: return ApiColletctionResponse in controller

--- tjf-api-samples/tjf-api-jpa-filters-sample/src/main/java/com/tjf/sample/github/apijpa/filter/controller/SFController.java
+++ tjf-api-samples/tjf-api-jpa-filters-sample/src/main/java/com/tjf/sample/github/apijpa/filter/controller/SFController.java
@@ -1,9 +1,5 @@
 package com.tjf.sample.github.apijpa.filter.controller;
 
-import com.tjf.sample.github.apijpa.filter.model.AccountModel;
-import com.tjf.sample.github.apijpa.filter.model.AccountModelRepository;
-import com.tjf.sample.github.apijpa.filter.model.EmployeeModel;
-import com.tjf.sample.github.apijpa.filter.model.EmployeeModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,13 +7,19 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//
-//import com.totvs.tjf.api.context.stereotype.ApiGuideline;
-//import com.totvs.tjf.api.context.stereotype.ApiGuideline.ApiGuidelineVersion;
+
+import com.tjf.sample.github.apijpa.filter.model.AccountModel;
+import com.tjf.sample.github.apijpa.filter.model.AccountModelRepository;
+import com.tjf.sample.github.apijpa.filter.model.EmployeeModel;
+import com.tjf.sample.github.apijpa.filter.model.EmployeeModelRepository;
+
+import com.totvs.tjf.api.context.stereotype.ApiGuideline;
+import com.totvs.tjf.api.context.stereotype.ApiGuideline.ApiGuidelineVersion;
+import com.totvs.tjf.api.context.v2.response.ApiCollectionResponse;
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-//@ApiGuideline(ApiGuidelineVersion.V2)
+@ApiGuideline(ApiGuidelineVersion.V2)
 public class SFController {
 
 	@Autowired
@@ -27,13 +29,13 @@ public class SFController {
 	private AccountModelRepository accountRepos;
 
 	@GetMapping(path = "/accounts")
-	public Page<AccountModel> getAllAccounts(Pageable pageable, AccountModel simpleFilter) {
-		return accountRepos.findAll(simpleFilter, pageable);
+	public ApiCollectionResponse<AccountModel> getAllAccounts(Pageable pageable, AccountModel simpleFilter) {
+		return ApiCollectionResponse.from(accountRepos.findAll(simpleFilter, pageable).toList());
 	}
 
 	@GetMapping(path = "/employees")
-	public Page<EmployeeModel> getAllEmployess(Pageable pageable, EmployeeModel simpleFilter) {
-		return employeeRepos.findAll(simpleFilter, pageable);
+	public ApiCollectionResponse<EmployeeModel> getAllEmployess(Pageable pageable, EmployeeModel simpleFilter) {
+		return  ApiCollectionResponse.from(employeeRepos.findAll(simpleFilter, pageable).toList());
 	}
 
 }
