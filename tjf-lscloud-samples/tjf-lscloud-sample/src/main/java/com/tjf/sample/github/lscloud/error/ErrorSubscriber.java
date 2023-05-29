a58c12688f74d6f9e6769bf9aa414912cffb4cb4
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2023-05-29 10:14:21 -0300

    fix(tjf-lscloud-samples): Ajuste para compatibilizar com o Spring Boot 3 (#60)
    * fix: ajuste lscloud
    
    * fix: ajuste na versão do TJF e application.ymal
    
    * Update ErrorSubscriber.java
    
    ---------
    
    Co-authored-by: Mauricio Holler Guntzel <mauricio.guntzel@totvs.com.br>

--- tjf-lscloud-samples/tjf-lscloud-sample/src/main/java/com/tjf/sample/github/lscloud/error/ErrorSubscriber.java
+++ tjf-lscloud-samples/tjf-lscloud-sample/src/main/java/com/tjf/sample/github/lscloud/error/ErrorSubscriber.java
@@ -1,26 +1,27 @@
 package com.tjf.sample.github.lscloud.error;
 
+import java.util.function.Consumer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import com.totvs.tjf.messaging.context.TOTVSMessage;
-import com.totvs.tjf.lscloud.messaging.LscloudExchange;
-import com.totvs.tjf.messaging.WithoutTenant;
 
-import static com.totvs.tjf.lscloud.messaging.LscloudExchangeChannel.Channel.INPUT;
-
-@EnableBinding(LscloudExchange.class)
+@Component
 public class ErrorSubscriber {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ErrorSubscriber.class);
 
-	@StreamListener(value = INPUT)
-	@WithoutTenant(ignore = true)
-	public void error(TOTVSMessage<?> message) {
-		LOG.info("Lscloud error received:\nType: {}\nContent: {}", 
-				message.getHeader().getType(),
-				message.getContent());
+	@Bean(name = "com.totvs.tjf.lscloud.message.LogLicense")
+	public Consumer<TOTVSMessage<?>> ErrorMessage() {
+		return message -> {
+			LOG.info("Lscloud error received:\nType: {}\nContent: {}", 
+					message.getHeader().getType(),
+					message.getContent());};
+
 	}
+	
+
 }
