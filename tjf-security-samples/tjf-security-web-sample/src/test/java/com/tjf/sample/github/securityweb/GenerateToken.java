b15ce0a5fda17b9969755ac259689dece90d01d2
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2023-09-04 13:48:39 -0300

    Fix(tjf-security-samples): ajuste teste unitario RAC (#65)
    * Fix(tjf-security-samples): ajuste teste unitario RAC

--- tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/GenerateToken.java
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/GenerateToken.java
@@ -5,21 +5,19 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-import org.springframework.http.MediaType;
 
 public class GenerateToken {
-	
+
 	public String generateToken(String url, String clientId, String clientSecret, String username, String password) throws ClientProtocolException {
 		String token = "";
 		
 		try (CloseableHttpClient client = HttpClients.createDefault()) {
-			StringEntity text = new StringEntity("client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=password&username=" + username + "&password=" + password + "&scope=authorization_api&acr_values=tenant:empresa1");
+			StringEntity text = new StringEntity("client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=password&username=" + username + "&password=" + password + "&scope=authorization_api");
 			
 			HttpPost post = new HttpPost(url);
 			post.setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
@@ -39,40 +37,6 @@ public class GenerateToken {
 		}
 		
 		return token;
-	}
-
-	/*
-	 * Updates the profile in rac, including the second role (2), which was imported by the plugin. 
-	 */
-	public void updateRolesUser(String url, String accessApplicationToken, String productRoles) {
-		try (CloseableHttpClient client = HttpClients.createDefault()) {
-			StringEntity params = new StringEntity("{\n" + 
-					"    \"id\": 2,\n" + 
-					"    \"userName\": \"admin\",\n" + 
-					"    \"emailAddress\": \"admin@seudominio.com.br\",\n" + 
-					"    \"fullName\": \"Administrador do Tenant\",\n" + 
-					"    \"name\": \"Administrador\",\n" + 
-					"    \"surname\": \"do Tenant\",\n" + 
-					"    \"phoneNumber\": null,\n" + 
-					"    \"isUndefinedPassword\": false,\n" + 
-					"    \"sentEmailSuccessfully\": null,\n" + 
-					"    \"isActive\": true,\n" + 
-					"    \"externalId\": null,\n" + 
-					"    \"organizations\": [],\n" + 
-					"    \"roles\": [2],\n" + 
-					"    \"productRoles\": [" + productRoles + "],\n" + 
-					"    \"_expandables\": []\n" + 
-					"}");
-			
-			HttpPut put = new HttpPut(url + "/api/user/2");
-			put.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken);
-			put.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-			put.setEntity(params);
-
-			client.execute(put);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
 	}
 
 }
--- tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/SecurityWebIT.java
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/SecurityWebIT.java
@@ -8,6 +8,7 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.client.ClientProtocolException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,38 +21,47 @@ public class SecurityWebIT {
 	private MockMvc mockMvc;
 
 	private static String accessApplicationToken;
-	private static final String URL_CONNECT = "http://rac:8080/totvs.rac/connect/token";
-	private static final String URL_RAC = "http://rac:8080/totvs.rac/";
-	private static final String CLIENT_ID = "js_oidc_sampleapp";
-	private static final String CLIENT_SECRET = "totvs@123";
-	
-	private static final String SUPERVISOR_ROLE = "1";
-	private static final String NO_ROLE = "0";
+	private static final String URL_CONNECT = "https://tenant-c56bab97-8ea6-4b6c-8568-1cde9c6a9477.rac.dev.totvs.app/totvs.rac/connect/token";
 
-	private void updateRoles(String roles) {
+	@Value("${tjf.security.clientid}")
+	private String CLIENT_ID;
+
+	@Value("${tjf.security.clientsecret}")
+	private String CLIENT_SECRET;
+
+	@Value("${tjf.security.login}")
+	private String LOGIN;
+
+	@Value("${tjf.security.password}")
+	private String PASSWORD;
+
+	@Value("${tjf.security.login-superv}")
+	private String LOGIN_SUPERV;
+
+	@Value("${tjf.security.password-superv}")
+	private String PASSWORD_SUPERV;
+
+	public void generateToken() throws ClientProtocolException {
 		GenerateToken racAuthorization = new GenerateToken();
+		accessApplicationToken = racAuthorization.generateToken(URL_CONNECT, CLIENT_ID, CLIENT_SECRET, LOGIN, PASSWORD);
+	}
 
-		try {
-			String updateApplicationToken = racAuthorization.generateToken(URL_CONNECT, CLIENT_ID, CLIENT_SECRET,
-					"admin", "totvs@123");
-			racAuthorization.updateRolesUser(URL_RAC, updateApplicationToken, roles);
-			accessApplicationToken = racAuthorization.generateToken(URL_CONNECT, CLIENT_ID, CLIENT_SECRET, "admin",
-					"totvs@123");
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		}
+	public void generateTokenSuperv() throws ClientProtocolException {
+		GenerateToken racAuthorization = new GenerateToken();
+		accessApplicationToken = racAuthorization.generateToken(URL_CONNECT, CLIENT_ID, CLIENT_SECRET, LOGIN_SUPERV,
+				PASSWORD_SUPERV);
 	}
 
 	@Test
 	public void testAccessWithTokenAndRole() throws Exception {
-		updateRoles(SUPERVISOR_ROLE);
+		generateTokenSuperv();
 		mockMvc.perform(get("/api/v1/machine").header(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	public void testAccessWithTokenNoRole() throws Exception {
-		updateRoles(NO_ROLE);
+		generateToken();
 		mockMvc.perform(get("/api/v1/machine").header(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken))
 				.andExpect(status().isOk());
 	}
@@ -65,12 +75,15 @@ public class SecurityWebIT {
 	public void testUnauthorizedAccess() throws Exception {
 		// TJF-1737 - Comportamento precisa ser avaliado
 		// mockMvc.perform(post("/api/v1/machine/stop")).andExpect(status().isUnauthorized());
+
+		generateToken();
 		mockMvc.perform(post("/api/v1/machine/stop")).andExpect(status().isForbidden());
 	}
 
 	@Test
 	public void testValidRole() throws Exception {
-		updateRoles(SUPERVISOR_ROLE);
+		generateTokenSuperv();
+
 		mockMvc.perform(
 				post("/api/v1/machine/stop").header(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken))
 				.andExpect(status().isOk());
@@ -78,7 +91,7 @@ public class SecurityWebIT {
 
 	@Test
 	public void testInvalidRole() throws Exception {
-		updateRoles(NO_ROLE);
+
 		mockMvc.perform(
 				post("/api/v1/machine/stop").header(HttpHeaders.AUTHORIZATION, "Bearer " + accessApplicationToken))
 				.andExpect(status().isForbidden());
