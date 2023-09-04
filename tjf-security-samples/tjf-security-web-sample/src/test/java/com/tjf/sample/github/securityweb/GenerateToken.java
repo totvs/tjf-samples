0353893a03d175b869be29ba5fc14bab7962d498
Mauricio Holler Guntzel <mauricio.guntzel@totvs.com.br>
2023-09-04 13:41:03 -0300

    fix: remoção comentarios

--- tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/GenerateToken.java
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/GenerateToken.java
@@ -5,38 +5,14 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-import org.springframework.http.MediaType;
 
 public class GenerateToken {
-	
-	
-	/*	RestTemplate restTemplate = new RestTemplate();
 
-		HttpHeaders headers = new HttpHeaders();
-
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("grant_type", "client_credentials");
-
-		headers.add(AUTHORIZATION,
-				getBasicAuthenticationHeader(application.getClientId(), application.getClientSecret()));
-		headers.add("Content-Type", "application/x-www-form-urlencoded");
-		headers.add(RESPONSE_TYPE, RESPONSE_TYPE_TOKEN);
-		headers.add(CLIENT_ID, application.getClientId());
-		headers.add(CLIENT_SECRET, application.getClientSecret());
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-		var endpoint = fluigUrl + "/accounts/oauth/token";
-
-		log.debug("Get token from {}", endpoint);
-		return restTemplate.exchange(endpoint, HttpMethod.POST, request, FluigTokenResponse.class).getBody();
-*/
-	
 	public String generateToken(String url, String clientId, String clientSecret, String username, String password) throws ClientProtocolException {
 		String token = "";
 		
