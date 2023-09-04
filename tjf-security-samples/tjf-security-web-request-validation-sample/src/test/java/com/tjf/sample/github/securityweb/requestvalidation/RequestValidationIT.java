0d4670e5d506f534c43b41e0f700c0c0a3096a49
Mauricio Holler Guntzel <mauricio.guntzel@totvs.com.br>
2023-09-04 15:28:41 -0300

    fix: ajuste teste unitario RAC

--- tjf-security-samples/tjf-security-web-request-validation-sample/src/test/java/com/tjf/sample/github/securityweb/requestvalidation/RequestValidationIT.java
+++ tjf-security-samples/tjf-security-web-request-validation-sample/src/test/java/com/tjf/sample/github/securityweb/requestvalidation/RequestValidationIT.java
@@ -1,17 +1,19 @@
 package com.tjf.sample.github.securityweb.requestvalidation;
 
-import com.tjf.sample.github.securityweb.requestvalidation.util.TokenUtil;
-import org.apache.http.HttpHeaders;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.tjf.sample.github.securityweb.requestvalidation.util.TokenUtil;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,11 +21,24 @@ public class RequestValidationIT {
 
 	@Autowired
 	private MockMvc mockMvc;
+	private static final String URL_CONNECT = "https://tenant-c56bab97-8ea6-4b6c-8568-1cde9c6a9477.rac.dev.totvs.app/totvs.rac/connect/token";
 
+	@Value("${tjf.security.clientid}")
+	private String CLIENT_ID;
+
+	@Value("${tjf.security.clientsecret}")
+	private String CLIENT_SECRET;
+
+	@Value("${tjf.security.login-superv}")
+	private String LOGIN_SUPERV;
+
+	@Value("${tjf.security.password-superv}")
+	private String PASSWORD_SUPERV;
+	
 	@Test
 	public void testParamAccessValid() throws Exception {
 		mockMvc.perform(get("/api/validation/param")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole()))
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT, CLIENT_ID , CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV)))
 			.andExpect(status().isOk())
 			.andExpect(content().string("Param"));
 	}
@@ -31,7 +46,7 @@ public class RequestValidationIT {
 	@Test
 	public void testHeaderAccessValid() throws Exception {
 		mockMvc.perform(get("/api/validation/header")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole()))
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT,CLIENT_ID,CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV)))
 			.andExpect(status().isOk())
 			.andExpect(content().string("Header"));
 	}
@@ -39,7 +54,7 @@ public class RequestValidationIT {
 	@Test
 	public void testBodyAccessValid() throws Exception {
 		mockMvc.perform(post("/api/validation/body")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole()))
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT,CLIENT_ID,CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV)))
 			.andExpect(status().isOk())
 			.andExpect(content().string("Body"));
 	}
@@ -47,7 +62,7 @@ public class RequestValidationIT {
 	@Test
 	public void testParamAccessDeniedOrganization() throws Exception {
 		mockMvc.perform(get("/api/validation/param")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole())
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT,CLIENT_ID,CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV))
 				.param("organization", "1"))
 			.andExpect(status().isForbidden());
 	}
@@ -55,7 +70,7 @@ public class RequestValidationIT {
 	@Test
 	public void testHeaderAccessDeniedOrganization() throws Exception {
 		mockMvc.perform(get("/api/validation/header")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole())
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT,CLIENT_ID,CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV))
 				.header("organization", "1"))
 			.andExpect(status().isForbidden());
 	}
@@ -63,8 +78,8 @@ public class RequestValidationIT {
 	@Test
 	public void testBodyAccessDeniedOrganization() throws Exception {
 		mockMvc.perform(post("/api/validation/body")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.createAccessSupervisorRole())
-				.content(" { \"organization\": \"1\" }"))
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + TokenUtil.generate(URL_CONNECT,CLIENT_ID,CLIENT_SECRET,LOGIN_SUPERV,PASSWORD_SUPERV))
+				.content("{ \"organization\": \"1\" }"))
 			.andExpect(status().isForbidden());
 	}
 }
