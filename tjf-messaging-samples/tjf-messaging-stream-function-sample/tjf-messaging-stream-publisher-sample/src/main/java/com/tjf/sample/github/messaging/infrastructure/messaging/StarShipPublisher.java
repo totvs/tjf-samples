af963221998dabcfb990b10eebccf6927d83b746
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2024-04-04 09:39:07 -0300

    Update StarShipPublisher.java

--- tjf-messaging-samples/tjf-messaging-stream-function-sample/tjf-messaging-stream-publisher-sample/src/main/java/com/tjf/sample/github/messaging/infrastructure/messaging/StarShipPublisher.java
+++ tjf-messaging-samples/tjf-messaging-stream-function-sample/tjf-messaging-stream-publisher-sample/src/main/java/com/tjf/sample/github/messaging/infrastructure/messaging/StarShipPublisher.java
@@ -3,7 +3,7 @@ package com.tjf.sample.github.messaging.infrastructure.messaging;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
 
-import com.totvs.tjf.messaging.context.TOTVSMessageBuilderFactory;
+import com.totvs.tjf.messaging.context.TOTVSMessageBuilder;
 
 @Component
 public class StarShipPublisher {
@@ -16,7 +16,7 @@ public class StarShipPublisher {
 
 	public <T> void publishEvent(T starShipEvent) {
 		System.out.println(starShipEvent.getClass().getSimpleName() + " enviado!");
-		var message = TOTVSMessageBuilderFactory.createCloudStreamTOTVSMessageBuilder()
+		var message = TOTVSMessageBuilder.stream()
 				.withType(starShipEvent.getClass().getSimpleName())
 				.setContent(starShipEvent)
 				.build();
