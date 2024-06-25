297a010c0c80744cf42f95349b009899d416aaad
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2024-06-25 16:11:41 -0300

    fix(tjf-messaging-stream): ajuste teste integrado (#71)
    Co-authored-by: Mauricio Holler Guntzel <mauricio.guntzel@totvs.com.br>

--- tjf-messaging-samples/tjf-messaging-stream-error-queue-sample/src/test/java/com/tjf/sample/github/messagingstream/MessagingStreamErrorQueueIT.java
+++ tjf-messaging-samples/tjf-messaging-stream-error-queue-sample/src/test/java/com/tjf/sample/github/messagingstream/MessagingStreamErrorQueueIT.java
@@ -1,35 +1,25 @@
 package com.tjf.sample.github.messagingstream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest(classes = MessagingStreamApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
+@SpringBootTest(classes = MessagingStreamApplication.class)
+@AutoConfigureMockMvc
 public class MessagingStreamErrorQueueIT {
-	
-	RestTemplate restTemplate = new RestTemplate();
 
-	@LocalServerPort
-	private int randomServerPort;
-	
-	//@Autowired
-	//private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-	//@Test
+	@Test
 	public void testsendMessage() throws Exception {
 		String expectedResult = "{\"messages\":1}";
 
@@ -39,39 +29,16 @@ public class MessagingStreamErrorQueueIT {
 				"  \"mission\": \"Get the location of Luke Skywalker\"\n" +
 				"}";
 
-		/*mockMvc.perform(post("/mission/send")
+		mockMvc.perform(post("/mission/send")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(jsonData))
-						.andExpect(status().isCreated());*/
-		
-      HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+						.andExpect(status().isCreated());
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(jsonData, headers);
+		Thread.sleep(5_000);
 
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                "http://localhost:" + randomServerPort + "/mission/send", // URL do endpoint
-                HttpMethod.POST, // Método HTTP POST
-                requestEntity,
-                String.class
-        );
-
-        // Verifica se o status da resposta é 201 Created
-        assert responseEntity.getStatusCodeValue() == HttpStatus.CREATED.value();
-
-        Thread.sleep(5_000);
-
-	/*mockMvc.perform(get("/actuator/messaging"))
+		mockMvc.perform(get("/actuator/messaging"))
 				.andExpect(status().isOk())
-				.andExpect(content().json(expectedResult));*/
-        
-        ResponseEntity<String> responseEntityActuator = restTemplate.getForEntity(
-        		"http://localhost:" + randomServerPort + "/actuator/messaging", // URL do endpoint
-                String.class
-        );
-
-        // Verifica se o status da resposta é 200 OK
-        assert responseEntity.getStatusCodeValue() == HttpStatus.OK.value();
+				.andExpect(content().json(expectedResult));
 	}
 
 }
