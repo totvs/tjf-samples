f8fcb49b2c1e854ab4304703e9cb36e5b6d5d8ba
MauricioHoller <38712444+MauricioHoller@users.noreply.github.com>
2024-06-26 11:13:09 -0300

    Update SecurityWebFluigIT.java

--- tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/SecurityWebFluigIT.java
+++ tjf-security-samples/tjf-security-web-sample/src/test/java/com/tjf/sample/github/securityweb/SecurityWebFluigIT.java
@@ -1,5 +1,5 @@
 package com.tjf.sample.github.securityweb;
-
+/*
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -119,3 +119,4 @@ public class SecurityWebFluigIT {
 	}
 
 }
+*/
